5f63c1d7629443e54030e8e30d0507eb3fccc6a9
Stefan Palm <stefan.palm@hotmail.se>
2019-09-09 15:21:29 +0200

    saves csv when quiting

--- myRecorder.py
+++ myRecorder.py
@@ -9,25 +9,33 @@ frameCounter = 0
 # start working with the camera, using default TBD add camera number as arg
 cap = cv2.VideoCapture(0)
 
+#collecting some information and to be save in a csv file
+logfile = np.array([["Frame Number","Timestamp"]])
+
 while(True):
     #get frame-by-frame
     ret, frame = cap.read()
 
-    #ste-up frame counter
+    #set-up frame counter
     frameCounter += 1
 
     #let add some information to the frame
     timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     frameNumber = str(frameCounter)
     displayText = timestamp + '  ' + frameNumber
-
     cv2.putText(frame,displayText,(130,30), cv2.FONT_HERSHEY_PLAIN, 1,(0,0,255),2,cv2.LINE_AA)
-
+  
     #Display the frame
     cv2.imshow('Camera feed', frame)
 
+    #save info to logfile
+    logfile = np.append(logfile, [[frameNumber,timestamp]], axis=0)
+
     #give the user an option to do something, q for quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        
+        np.savetxt('%s.csv' % timestamp, logfile, delimiter=',', fmt='%s')
+
         break
 
 # Clean up as we are leaving
